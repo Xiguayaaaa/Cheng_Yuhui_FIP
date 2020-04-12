ff8cd1825f7472486a60e773a4a9a78cbe1150f9
Yuhui Cheng <46694948+Xiguayaaaa@users.noreply.github.com>
2020-04-13 01:27:52 +0800

    add

---
+++ js/main.js
@@ -0,0 +1,14 @@
+(() => {
+	console.log("Rolex");
+
+	var button = document.querySelector("#nav-button i");
+	var mainNav = document.querySelector("#main-nav");
+
+	function showMenu() {
+		mainNav.classList.toggle("slideToggle");
+		button.classList.toggle("expanded");
+	}
+
+	button.addEventListener("click", showMenu, false);
+
+})();
